dc1cf416cf3b3cd431149c27204361a3373bbe0f
AndreyBayrak <andrey-yalta@e-mail.ua>
2015-08-30 20:13:03 +0300

    Create JSON.java

---
+++ JSON.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class JSON {
+    public String name;
+    public String surname;
+    public String[] phones;
+    public String[] sites;
+    public Address address;
+}
